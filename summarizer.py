a356f54cf9bcdc28f308e3b4dbbf5acb54a48774
moatasem mohammed <47195525+moatasem75291@users.noreply.github.com>
2024-04-23 18:51:40 +0200

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,12 +1,11 @@
 import requests
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 headers = {"Authorization": "Bearer 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"}
 
 def summarize_text(text):
     payload = {
     "inputs": text,
-    "parameters": {"candidate_labels": ["refund", "legal", "faq"]},
     }
     response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+    return response.json()[0]
