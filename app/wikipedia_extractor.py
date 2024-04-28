e42794309d61297c0a7d787c15c9f5a1349d274f
Mo3tsm <mo5les75291@gmail.com>
2024-04-28 04:30:30 +0300

    dealing with the error messages

--- app/wikipedia_extractor.py
+++ app/wikipedia_extractor.py
@@ -21,6 +21,13 @@ def extract_text_from_wikipedia(title_or_url):
         text = page.content
         return text, article_title
     except wikipedia.exceptions.DisambiguationError as e:
-        raise ValueError(f"Disambiguation error: {e} 😞")
+        error_message = f"**Disambiguation Error** \n\n"
+        error_message += f"The title '{article_title}' is ambiguous and can refer to multiple pages.😞\n\n"
+        error_message += f"Please try to disambiguate the title by selecting one of the following options:\n\n"
+        error_message += f"{e.options}\n"
+        return Exception(error_message), None
     except wikipedia.exceptions.PageError:
-        raise ValueError(f"Page not found: {article_title} 🤔")
+        error_message = f"**Page Not Found**\n"
+        error_message += f"The page '{article_title}' was not found on Wikipedia. 🤔\n"
+        error_message += f"Please check the title and try again.\n"
+        return Exception(error_message), None
